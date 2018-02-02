90af30bbe9861dae409e284d23cc377a1abf815f
Jhuni0123 <whdgnsdl887@naver.com>
2018-02-03 02:20:12 +0900

    Implement fetch_memory_usage

--- apps/client/lib/metric.ex
+++ apps/client/lib/metric.ex
@@ -4,7 +4,36 @@ defmodule Metric do
   end
 
   def fetch_memory_usage do
-    :memory
+    {output, status} = System.cmd("cat", ["/proc/meminfo"])
+
+    parse_line = fn line ->
+      try do
+        [key | tail] = line |> String.split(":")
+        value = tail |> List.first |> String.trim |> String.split |> List.first
+        {:ok, %{key: key, value: String.to_integer(value)}}
+      rescue
+        e in MatchError ->
+          {:error, e}
+      end
+    end
+
+    case {output, status} do
+      {output, 0} ->
+        lines = output |> String.trim |> String.split("\n")
+        fold = fn (res, map) ->
+          case res do
+            {:ok, %{key: key, value: value}} ->
+              Map.put(map, key, value)
+            {:error, _} ->
+              map
+          end
+        end
+        meminfo_map = lines |> Enum.map(parse_line) |> List.foldl(%{}, fold)
+        meminfo_map
+
+      _ ->
+        %{}
+    end
   end
 
   def fetch_disk_usage do
