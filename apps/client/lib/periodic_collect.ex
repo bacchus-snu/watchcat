ba27ae9cdfeb7d06df5cd9d78cde88f51cb443f0
skystar <wogus150@naver.com>
2018-02-04 02:33:12 +0900

    cpu calculation

--- apps/client/lib/periodic_collect.ex
+++ apps/client/lib/periodic_collect.ex
@@ -6,14 +6,17 @@ defmodule PeriodicCollect do
   end
 
   def init(state) do
-    File.write("/home/skystar/yes", "yo")
+    # Setup ETS table
+    :ets.new(:metric, [:named_table])
+
     metric_collection()
     {:ok, state}
   end
 
   def handle_info(:collect_metric, state) do
     # do something...
-    Task.start_link(&task_collect_metric/0)
+    # TODO: make below ASYNC
+    task_collect_metric()
     metric_collection()
     {:noreply, state}
   end
@@ -23,7 +26,45 @@ defmodule PeriodicCollect do
   end
 
   defp task_collect_metric() do
-    IO.puts "working!"
+    metrics = [
+      {:cpu_metric, Metric.fetch_cpu_usage},
+      {:memory_metric, Metric.fetch_memory_usage},
+      {:disk_metric, Metric.fetch_disk_usage},
+      {:network_metric, Metric.fetch_network_usage},
+    ]
+
+    [cpu, memory, disk, network] = metrics
+
+    # cpu |> inspect |> IO.puts
+    :ets.insert(:metric, {:cpu, calculate_cpu_usage(cpu)})
+    # memory |> inspect |> IO.puts
+    # disk |> inspect |> IO.puts
+    # network |> inspect |> IO.puts
+
+    :ets.insert(:metric, metrics)
+  end
+
+  defp calculate_cpu_usage({:cpu_metric, curr}) do
+    case :ets.lookup(:metric, :cpu_metric) |> List.first do
+      nil ->
+        :not_available
+      {:cpu_metric, prev} ->
+        for {prev_core, curr_core} <- Enum.zip(prev, curr),
+          name = prev_core["name"],
+          idle = curr_core["idle"] - prev_core["idle"],
+          total = curr_core["total"] - prev_core["total"],
+          usage_percent = ((total - idle) / total) * 100 do
+            %{"name" => name, "usage" => usage_percent}
+          end
+    end
+  end
+
+  defp calculate_memory_usage({:memory_metric, metric}) do
+
+  end
+
+  defp calculate_disk_usage(metric) do
+
   end
 
   defp metric_collection() do
