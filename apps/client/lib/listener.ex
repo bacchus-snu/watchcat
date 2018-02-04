4e3cf2b024d04a052aa05237d4910f37c3ef4d52
skystar <wogus150@naver.com>
2018-02-04 21:17:57 +0900

    Listener implemented

--- apps/client/lib/listener.ex
+++ apps/client/lib/listener.ex
@@ -29,7 +29,6 @@ defmodule Listener do
         :ok = :gen_tcp.controlling_process(incoming, pid)
         accepter_loop(socket)
       {:error, _reason} ->
-        Logger.info "error occurred in accept()"
         accepter_loop(socket)
     end
   end
@@ -37,24 +36,45 @@ defmodule Listener do
   defp serve_request(socket) do
     case :gen_tcp.recv(socket, 0) do
       {:ok, data} ->
-        handle_data(data)
+        handle_data(socket, data)
         serve_request(socket)
       {:error, _reason} -> nil
     end
   end
 
-  defp handle_data(packed_data) do
+  defp handle_data(socket, packed_data) do
     [command | args] = unpack(packed_data)
     case command do
       "metric" ->
-        :not_implemented
+        data = args |> Enum.map(&fetch_metric/1) |> Map.new() |> pack()
+        :gen_tcp.send(socket, data)
       _ ->
         :error
     end
   end
 
+  defp fetch_metric(key) do
+    key_atom = case key do
+      "cpu" -> :cpu
+      "memory" -> :memory
+      "disk" -> :disk
+      "network" -> :network
+      "uptime" -> :uptime
+    end
+
+    :ets.lookup(:metric, key_atom) |> List.first()
+  end
+
+  defp pack(data) do
+    data
+    |> Msgpax.pack!()
+    |> IO.iodata_to_binary()
+    |> Base.encode64
+  end
+
   defp unpack(data) do
     data
+    |> String.trim()
     |> Base.decode64!()
     |> Msgpax.unpack!()
   end
