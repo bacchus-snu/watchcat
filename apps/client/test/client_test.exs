d1b8d16617545ba1dfa690fb7da6767a081d8cfe
skystar-p <usezmap@gmail.com>
2018-02-05 11:20:08 +0900

    please write some tests!

--- apps/client/test/client_test.exs
+++ apps/client/test/client_test.exs
@@ -2,6 +2,7 @@ defmodule ClientTest do
   use ExUnit.Case
   doctest Client
 
+  # TODO: write some tests!
   test "greets the world" do
     assert Client.hello() == :world
   end
--- apps/server/test/server_test.exs
+++ apps/server/test/server_test.exs
@@ -2,6 +2,7 @@ defmodule ServerTest do
   use ExUnit.Case
   doctest Server
 
+  # TODO: write some tests!
   test "greets the world" do
     assert Server.hello() == :world
   end
