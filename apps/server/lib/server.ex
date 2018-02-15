3f3c6e28e439a89d5f28dd0a1b6adbeb150fd528
skystar <wogus150@naver.com>
2018-02-16 00:55:53 +0900

    secret key generate

--- apps/server/lib/server.ex
+++ apps/server/lib/server.ex
@@ -1,8 +1,16 @@
 defmodule Server do
   use Application
+  require Logger
   require ServerSupervisor
 
   def start(_type, _args) do
+    init_secret_key()
+    start_cowboy()
+    ServerSupervisor.start_link([])
+  end
+
+  defp start_cowboy() do
+    Logger.info("Starting cowboy")
     router = :cowboy_router.compile([
       {:"_", [
         {"/api/metric/[:machine]", HTTPHandler.MetricReq, []},
@@ -14,7 +22,19 @@ defmodule Server do
       [port: 10102],
       %{env: %{dispatch: router}}
     )
+  end
 
-    ServerSupervisor.start_link([])
+  defp init_secret_key() do
+    priv_path = Application.app_dir(:server, "priv")
+    secret_key_path = Path.join(priv_path, "secret_key")
+
+    unless File.exists?(secret_key_path) do
+      Logger.info("Generating new secret key")
+      charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      random_secret = charset |> Enum.take_random(64) |> to_string()
+
+      File.mkdir_p!(priv_path)
+      File.write(secret_key_path, random_secret)
+    end
   end
 end
