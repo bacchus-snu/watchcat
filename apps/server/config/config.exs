19f35a8d4118bdfcbf14a861223bdba364ddda18
skystar <wogus150@naver.com>
2018-02-17 01:41:51 +0900

    cert and key path config

--- apps/server/config/config.exs
+++ apps/server/config/config.exs
@@ -2,7 +2,9 @@ use Mix.Config
 
 config :server, :general,
   db_filename: "client_db",
-  crawl_interval: 3000
+  crawl_interval: 3000,
+  cert_path: nil,
+  key_path: nil
 
 config :server, :network,
   client_port: 10101,
