9128a930f1a888850e3ee8f3d66e55145f98e4c6
skystar <wogus150@naver.com>
2018-02-27 19:00:58 +0900

    No umbrella

--- .formatter.exs
+++
@@ -1,4 +0,0 @@
-# Used by "mix format"
-[
-  inputs: ["mix.exs", "apps/*/mix.exs", "apps/*/{config,lib,test}/**/*.{ex,exs}"]
-]
--- apps/client/mix.exs
+++ apps/client/mix.exs
@@ -5,10 +5,10 @@ defmodule Client.MixProject do
     [
       app: :client,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
+      build_path: "./_build",
+      config_path: "./config/config.exs",
+      deps_path: "./deps",
+      lockfile: "./mix.lock",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -16,7 +16,6 @@ defmodule Client.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       mod: {Client, []},
@@ -24,7 +23,6 @@ defmodule Client.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:msgpax, "~>2.0"},
