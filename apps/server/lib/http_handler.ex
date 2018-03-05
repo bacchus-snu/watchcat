b5a27c3b976d6ba0e95ca2446d6c2379b787ccb3
Jhuni0123 <whdgnsdl887@naver.com>
2018-03-05 17:26:18 +0900

    Add post, delete tag api

--- apps/server/lib/http_handler.ex
+++ apps/server/lib/http_handler.ex
@@ -223,9 +223,64 @@ defmodule HTTPHandler.MachineTagReq do
     else
       {:ok, body, req1} = :cowboy_req.read_body(req0, %{length: 1024})
       new_tags = body |> Poison.decode!()
+      true = is_list(new_tags) and Enum.all?(new_tags, &is_binary/1)
 
       machine_name = :cowboy_req.binding(:machine_name, req1)
       [{^machine_name, old}] = :dets.lookup(:clients, machine_name)
+      new = old |> Map.put("tags", new_tags)
+
+      :ok = :dets.insert(:clients, {machine_name, new})
+
+      req = :cowboy_req.reply(200, %{"content-type" => "text/plain"}, "", req1)
+      {:ok, req, state}
+    end
+  rescue
+    _ ->
+      req = :cowboy_req.reply(400, %{"content-type" => "text/plain"}, "", req0)
+      {:ok, req, state}
+  end
+
+  def init(req0 = %{method: "POST"}, state) do
+    permission = req0 |> HTTPHandler.get_permission()
+
+    if permission != "admin" do
+      req = :cowboy_req.reply(403, %{"content-type" => "text/plain"}, "", req0)
+      {:ok, req, state}
+    else
+      {:ok, body, req1} = :cowboy_req.read_body(req0, %{length: 1024})
+      new_tag = body |> Poison.decode!()
+      true = is_binary(new_tag)
+
+      machine_name = :cowboy_req.binding(:machine_name, req1)
+      [{^machine_name, old}] = :dets.lookup(:clients, machine_name)
+      new_tags = [new_tag | old["tags"]] |> Enum.uniq()
+      new = old |> Map.put("tags", new_tags)
+
+      :ok = :dets.insert(:clients, {machine_name, new})
+
+      req = :cowboy_req.reply(200, %{"content-type" => "text/plain"}, "", req1)
+      {:ok, req, state}
+    end
+  rescue
+    _ ->
+      req = :cowboy_req.reply(400, %{"content-type" => "text/plain"}, "", req0)
+      {:ok, req, state}
+  end
+
+  def init(req0 = %{method: "DELETE"}, state) do
+    permission = req0 |> HTTPHandler.get_permission()
+
+    if permission != "admin" do
+      req = :cowboy_req.reply(403, %{"content-type" => "text/plain"}, "", req0)
+      {:ok, req, state}
+    else
+      {:ok, body, req1} = :cowboy_req.read_body(req0, %{length: 1024})
+      tag = body |> Poison.decode!()
+      true = is_binary(tag)
+
+      machine_name = :cowboy_req.binding(:machine_name, req1)
+      [{^machine_name, old}] = :dets.lookup(:clients, machine_name)
+      new_tags = old["tags"] |> List.delete(tag)
       new = old |> Map.put("tags", new_tags)
 
       :ok = :dets.insert(:clients, {machine_name, new})
