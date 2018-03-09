7ce16892ab49f684da5c7ef9c585cf9df27fd16e
Jhuni0123 <whdgnsdl887@naver.com>
2018-03-10 02:01:51 +0900

    Create machines table

---
+++ apps/frontend/components/MachineRow.vue
@@ -0,0 +1,12 @@
+<template>
+  <tr>
+    <td>{{ info.name }}</td>
+    <td>{{ info.host }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: ['info']
+}
+</script>`
--- apps/frontend/pages/machines/index.vue
+++ apps/frontend/pages/machines/index.vue
@@ -1,3 +1,42 @@
 <template>
-  <div>Machines page</div>
+  <div>
+    <p>Machines page</p>
+    <table id='machines-table'>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>host</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr is='machine-row'
+          v-for='machine in machine_list'
+          :info='machine'
+          :key='machine.name'
+        ></tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
+<script>
+import MachineRow from '~/components/MachineRow.vue'
+import axios from 'axios'
+
+export default {
+  data () {
+    machine_list: []
+  },
+  asyncData () {
+    return axios.get('http://watchcat.bacchus.snucse.org:10102/api/machines')
+      .then(function(res) {
+        return {
+          machine_list: res.data
+        }
+      })
+  },
+  components: {
+    MachineRow
+  }
+}
+</script>
