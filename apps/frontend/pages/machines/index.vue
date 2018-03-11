a2f5dbb7740beac72e5b758daddc187f7e6de74b
Jhuni0123 <whdgnsdl887@naver.com>
2018-03-12 03:32:23 +0900

    Replace machine table with element-ui table

--- apps/frontend/pages/machines/index.vue
+++ apps/frontend/pages/machines/index.vue
@@ -3,38 +3,57 @@
     <p>Machines page</p>
     <input type='checkbox' id='auto-refresh' v-model='auto_refresh'>
     <label for='auto-refresh'>자동 새로고침</label>
-    <table id='machines-table'>
-      <thead>
-        <tr>
-          <th>name</th>
-          <th>host</th>
-          <th>status</th>
-          <th>CPU</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr is='machine-row'
-          v-for='machine in machine_list'
-          :info='machine'
-          :metric='metric_map[machine.name]'
-          :key='machine.name'
-        ></tr>
-      </tbody>
-    </table>
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+      :cell-style="{height: '10px', padding: '5px'}"
+      :default-sort="{prop: 'name'}"
+      row-key="name"
+    >
+      <el-table-column
+        fixed
+        :sortable="true"
+        prop="name"
+        label="Name"
+      />
+      <el-table-column
+        prop="host"
+        label="Host"
+      />
+      <el-table-column
+        :sortable="true"
+        prop="status"
+        label="Status"
+      />
+      <el-table-column
+        v-for="column in metricColumns"
+        :prop="column"
+        :label="capitalize(column)"
+        :key="column"
+      />
+    </el-table>
   </div>
 </template>
 
 <script>
-import MachineRow from '~/components/MachineRow.vue'
-
 export default {
   data () {
     return {
       machine_list: [],
       metric_map: {},
-      auto_refresh: false
+      auto_refresh: false,
+      metricColumns: [
+        "cpu"
+      ]
     }
   },
+
+  computed: {
+    tableData () {
+      return this.machine_list.map((machine) => Object.assign(machine, this.metricToRow(this.metric_map[machine.name])))
+    }
+  },
+
   async asyncData ({ app }) {
     let machine_list, metric_list
     [machine_list, metric_list] = await Promise.all([
@@ -42,17 +61,14 @@ export default {
       app.$axios.$get('/api/metric')
     ])
     return {
-      machine_list: machine_list.sort(function(a,b) {
-        if (a.name > b.name)return 1
-        else if (a.name < b.name)return -1
-        else return 0
-      }),
+      machine_list,
       metric_map: metric_list.reduce(function(acc, metric) {
         acc[metric.name] = metric;
         return acc
       }, {})
     }
   },
+
   created () {
     this.fetchMetric()
     setInterval(function () {
@@ -65,20 +81,41 @@ export default {
     async fetchMetric () {
       var metric_list = await this.$axios.$get('/api/metric')
       this.metric_map = metric_list.reduce(function(acc, metric) {acc[metric.name] = metric; return acc}, {})
+    },
+
+    metricToRow (metric) {
+      let row = this.metricColumns.reduce(function(map, column) {
+        map[column] = "-"
+        return map
+      }, {})
+
+      row.status = metric.status == "ok" ? "O" : "X"
+
+      if (metric.status == "error") {
+        return row
+      }
+      let self = this
+      this.metricColumns.forEach(function(column) {
+        row[column] = self.metricToCell(column, metric.data)
+      })
+      return row
+    },
+
+    metricToCell (column, metric) {
+      if (metric[column].status == "error") {
+        return "-"
+      }
+
+      let data = metric[column].data
+
+      if (column == "cpu") {
+        return data[0].usage.toFixed(2) + "%"
+      }
+    },
+
+    capitalize (s) {
+      return s[0].toUpperCase() + s.slice(1)
     }
-  },
-  components: {
-    MachineRow
   }
 }
 </script>
-
-<style>
-table {
-  border-collapse: collapse;
-}
-
-table, th, td {
-  border: 1px solid black;
-}
-</style>
