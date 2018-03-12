8cee45ca2ec0fadf9a76c9d1a6a7ce6eed831217
Jhuni0123 <whdgnsdl887@naver.com>
2018-03-12 17:36:04 +0900

    Add nav menu

--- apps/frontend/layouts/default.vue
+++ apps/frontend/layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
   <el-container>
-    <el-header>
+    <el-header height="100px">
       <h1>WatchCat</h1>
+      <el-menu :router="true" :default-active="$route.path" class="menu-bar" mode="horizontal">
+        <el-menu-item index="/machines">Machines</el-menu-item>
+        <el-menu-item index="/tags">Tags</el-menu-item>
+      </el-menu>
     </el-header>
     <el-main>
       <nuxt/>
