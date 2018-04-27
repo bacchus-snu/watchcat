ecf87d275adf9891b58622d8f91fed5d9ec1f887
skystar-p <usezmap@gmail.com>
2018-04-28 05:15:14 +0900

    diskcard skeleton

---
+++ apps/frontend/components/DiskCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-card>
+    <h2 slot="header">Disk</h2>
+    <div class="card-body">
+      <template v-if="disk.ok">
+
+      </template>
+      <template v-else>
+        <p>{{ disk.reason }}</p>
+      </template>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: ["metric"],
+
+  computed: {
+    disk () {
+      let diskInfo = this.metric
+      let ret = {}
+      ret.ok = diskInfo.status === "ok"
+      if (ret.ok) {
+        // do something...
+      } else {
+        ret.reason = diskInfo.reason
+      }
+
+      return ret
+    }
+  }
+}
+</script>
